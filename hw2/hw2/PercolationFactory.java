232495584650e740d6acd6b889dde1ff8a9da4d7
Willk1209 <jyk980803171@qq.com>
2023-03-30 19:04:03 +0800

    HW2 finished

--- hw2/hw2/PercolationFactory.java
+++ hw2/hw2/PercolationFactory.java
@@ -1,8 +1,5 @@
 package hw2;
 
-import java.util.ArrayList;
-import java.util.Random;
-
 public class PercolationFactory {
     public Percolation make(int N) {
         return new Percolation(N);
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -8,26 +8,26 @@ public class PercolationStats {
     private final int T;
     private Percolation pc;
     private double[] thresholds;
-    PercolationStats (int N, int T, PercolationFactory pf) {
+    PercolationStats(int N, int T, PercolationFactory pf) {
         this.N = N;
         this.T = T;
         this.thresholds = new double[T];
 
         ArrayList samplesList = new ArrayList();
-        for (int i=0; i<N*N; i++) {
+        for (int i = 0; i < N * N; i++) {
             samplesList.add(i);
         }
         // Use MC simulation to iterate until the system is percolated.
         Random rand = new Random();
-        for (int iter_num = 0; iter_num < T; iter_num++) {
+        for (int iterNum = 0; iterNum < T; iterNum++) {
             this.pc = pf.make(N);
-            for (int i=0; i<N*N; i++) {
-                int sampleIndex = rand.nextInt(N*N);
+            for (int i = 0; i < N * N; i++) {
+                int sampleIndex = rand.nextInt(N * N);
                 int sampleRow = sampleIndex / N;
                 int sampleCol = sampleIndex % N;
                 pc.open(sampleRow, sampleCol);
                 if (pc.percolates()) {
-                    thresholds[iter_num] = (double) pc.numberOfOpenSites() / (N*N);
+                    thresholds[iterNum] = (double) pc.numberOfOpenSites() / (N * N);
                     break;
                 }
             }
@@ -38,14 +38,14 @@ public class PercolationStats {
         for (double currentNum : thresholds) {
             totalNum = totalNum + currentNum;
         }
-        return totalNum/T;
+        return totalNum / T;
     }
     public double stddev(double mean) {
         double totalDevSquare = 0.;
         for (double i : thresholds) {
             totalDevSquare = totalDevSquare + Math.pow((i - mean), 2);
         }
-        return Math.sqrt(totalDevSquare / (T-1));
+        return Math.sqrt(totalDevSquare / (T - 1));
     }
     public double confidenceLow(double mean, double stddev) {
         return (mean - 1.96 * stddev / Math.sqrt(T));
@@ -54,14 +54,14 @@ public class PercolationStats {
         return (mean + 1.96 * stddev / Math.sqrt(T));
     }
 
-    public static void main(String[] args) {
-        PercolationFactory pf = new PercolationFactory();
-        PercolationStats ps = new PercolationStats(10, 20, pf);
-        double mean = ps.mean();
-        System.out.println(mean);
-        double std = ps.stddev(ps.mean());
-        System.out.println(std);
-        System.out.println(ps.confidenceLow(mean, std));
-        System.out.println(ps.confidenceHigh(mean, std));
-    }
+//    public static void main(String[] args) {
+//        PercolationFactory pf = new PercolationFactory();
+//        PercolationStats ps = new PercolationStats(10, 20, pf);
+//        double mean = ps.mean();
+//        System.out.println(mean);
+//        double std = ps.stddev(ps.mean());
+//        System.out.println(std);
+//        System.out.println(ps.confidenceLow(mean, std));
+//        System.out.println(ps.confidenceHigh(mean, std));
+//    }
 }
